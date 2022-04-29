9b038862f04bf19ad0f9608278fdc8840804b778
Mostafa Emami <emami7495@gmail.com>
2022-04-29 16:35:34 +0430

    Add 'randomcolor' package

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
-  useColorScheme,
+  StyleSheet, Text,
+  useColorScheme, View,
 } from 'react-native';
-
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+  },
+});
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  const squareSideSize = 2;
+  let square = null;
 
+  function generateSquare() {
+    square = new Array(squareSideSize);
+    for (let i = 0; i < squareSideSize; i += 1) {
+      square[i] = new Array(squareSideSize);
+    }
+    //
+    let counter = 1;
+    for (let i = 0; i < squareSideSize; i += 1) {
+      for (let j = 0; j < squareSideSize; j += 1) {
+        square[i][j] = { number: counter, color: '#111111' };
+        counter += 1;
+      }
+    }
+    return <Text style={{ maxWidth: 200 }}>{JSON.stringify(square)}</Text>;
+  }
+
+  useEffect(() => {
+    generateSquare();
+  }, [squareSideSize]);
+  //
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      />
+      <View style={styles.container}>
+        {generateSquare()}
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
