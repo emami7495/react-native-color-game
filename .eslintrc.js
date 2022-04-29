0608264124eb4b2f733b4f0b5927b55ab7f1538d
Mostafa Emami <emami7495@gmail.com>
2022-04-30 00:05:11 +0430

    Refactor game ui & change .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  'env': {
+    'browser': true,
+    'es2021': true,
   },
-  extends: [
+  'extends': [
     'plugin:react/recommended',
-    'airbnb',
+    'google',
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
   },
-  plugins: [
+  'plugins': [
     'react',
   ],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  'rules': {
+    'no-mixed-spaces-and-tabs': 0, // disable rule
+    'no-tabs': 0,
+    'max-len': 'off',
+    'react/function-component-definition': [2, {namedComponents: 'arrow-function'}],
+    'linebreak-style': 0,
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
+    'react/jsx-indent': ['off', 'tab'],
+    'react/jsx-indent-props': ['off', 'tab'],
+    'react/jsx-filename-extension': 0,
+    'brace-style': ['error', 'stroustrup', {allowSingleLine: true}],
+    'import/no-unresolved': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    'no-nested-ternary': 0,
+    'no-shadow': 0,
+    'jsx-a11y/no-autofocus': 0,
+    'object-curly-newline': ['error', {
+      ObjectPattern: {
+        multiline: true,
+      },
+      ImportDeclaration: 'never',
+      ExportDeclaration: {
+        multiline: true, minProperties: 5,
+      },
+    }],
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,132 +1,123 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import {
-  SafeAreaView, StyleSheet, Text, TouchableOpacity, View,
-} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
     alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
     backgroundColor: '#ffffff',
   },
 });
-const randomColor = require('randomcolor'); // import the script
+const randomColor = require('randomcolor');
 
-function App() {
+const App = () => {
   const [squareSideSize] = useState(4);
-  const [square, setSquare] = useState(new Array(squareSideSize).fill(new Array(squareSideSize)));
+  const [squareUi, setSquareUi] = useState([]);
   const [colorArray, setColorArray] = useState(null);
-  const squareUi = [];
-  //
-  const generateRandomColors = useCallback(
-    () => {
-      const halfArray = randomColor({
-        luminosity: 'light',
-        format: 'rgba',
-        hue: 'blue',
-        alpha: 0.50,
-        count: (squareSideSize * squareSideSize) / 2,
-      });
-      const finalArray = halfArray.concat(halfArray);
-      finalArray.sort(() => {
-        switch (Math.floor(Math.random() * 3)) {
-          case 0:
-            return -1;
-          case 1:
-            return 0;
-          case 2:
-            return 1;
-          default:
-            break;
-        } return 0;
-      });
-      setColorArray(finalArray);
-    },
-    [],
-  );
+  const [square, setSquare] = useState(new Array(squareSideSize));
 
-  function renderItem(row, itemNum) {
-    return (
-      <TouchableOpacity
-        key={`${row}${itemNum}`}
-        onPress={() => {
-          const temp = [...square];
-          temp[row][itemNum].selected = true;
-          setSquare(temp);
-        }}
-        style={{
-          margin: 4,
-          width: 56,
-          height: 56,
-          borderRadius: 12,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: square[row][itemNum].color,
-          borderWidth: square[row][itemNum].selected ? 4 : 0,
-        }}
-      >
-        <Text
+  const generateRandomColors = useCallback(() => {
+    const halfArray = randomColor({
+      luminosity: 'light',
+      format: 'rgba',
+      hue: 'blue',
+      alpha: 0.75,
+      count: (squareSideSize * squareSideSize) / 2,
+    });
+    const finalArray = halfArray.concat(halfArray);
+    finalArray.sort(() => {
+      switch (Math.floor(Math.random() * 3)) {
+        case 0:
+          return -1;
+        case 1:
+          return 0;
+        case 2:
+          return 1;
+        default:
+          break;
+      } return 0;
+    });
+    setColorArray(finalArray);
+  },
+  [],
+  );
+  useEffect(() => {
+    const tmp = [...square];
+    if (colorArray) {
+      for (let i = 0; i < squareSideSize; i += 1) {
+        tmp[i] = new Array(squareSideSize);
+      }
+      let counter = 1;
+      for (let i = 0; i < squareSideSize; i += 1) {
+        for (let j = 0; j < squareSideSize; j += 1) {
+          tmp[i][j] = {
+            num: counter,
+            selected: false,
+            color: colorArray?.[counter - 1],
+          };
+          counter += 1;
+        }
+      }
+      setSquare(tmp);
+    }
+  }, [colorArray]);
+
+  useEffect(() => {
+    const temp = [];
+    for (let i = 0; i < squareSideSize; i += 1) {
+      const rowArray = [];
+      for (let j = 0; j < squareSideSize; j += 1) {
+        rowArray.push(<TouchableOpacity
+          key={`${i}${j}`}
+          onPress={() => {
+            const tmp = [...square]; tmp[i][j].selected = true;
+		      setSquare(tmp);
+          }}
           style={{
-            fontSize: 32,
-            fontWeight: '100',
+            margin: 4,
+            width: 56,
+		    height: 56,
+		    borderRadius: 12,
+		    alignItems: 'center',
+            justifyContent: 'center',
+		    backgroundColor: square?.[i]?.[j]?.color,
+            borderWidth: square?.[i]?.[j]?.selected ? 3 : 0,
+		    borderColor: '#49a910',
           }}
         >
-          {square[row][itemNum].num}
-        </Text>
-      </TouchableOpacity>
-    );
-  }
-  //
-  function renderRow(row) {
-    const rowArray = [];
-    for (let i = 0; i < squareSideSize; i += 1) {
-      rowArray.push(renderItem(row, i));
-    }
-    return <View style={{ flexDirection: 'row' }}>{rowArray}</View>;
-  }
-  //
-  function generateSquare() {
-    for (let i = 0; i < squareSideSize; i += 1) {
-      square[i] = new Array(squareSideSize);
-    }
-    //
-    let counter = 1;
-    for (let i = 0; i < squareSideSize; i += 1) {
-      for (let j = 0; j < squareSideSize; j += 1) {
-        square[i][j] = {
-          num: counter,
-          selected: false,
-          color: colorArray[counter - 1],
-        };
-        counter += 1;
+          <Text style={{fontSize: 32, fontWeight: '100'}}>
+            {square?.[i]?.[j]?.num}
+          </Text>
+        </TouchableOpacity>,
+        );
       }
+	  temp.push(<View key={i} style={{flexDirection: 'row'}}>{rowArray}</View>);
     }
-    //
-    for (let i = 0; i < squareSideSize; i += 1) {
-      squareUi.push(renderRow(i));
-    }
-    return (<View style={{ flexDirection: 'column' }}>{squareUi}</View>);
-  }
-  //
-  useEffect(() => {
+    setSquareUi([...temp]);
   }, [square]);
-  //
+
   useEffect(() => {
     generateRandomColors();
   }, []);
-  //
-  if (colorArray) {
-    return (
-      <SafeAreaView>
-        <View style={styles.container}>
-          {generateSquare()}
-        </View>
-      </SafeAreaView>
-    );
-  }
-  return <View />;
-}
 
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text
+          style={{
+            textAlign: 'center',
+            margin: 16,
+            fontWeight: '200',
+            fontSize: 22,
+          }}>
+          Click On Similar Colors
+        </Text>
+        {squareUi}
+      </View>
+    </SafeAreaView>
+  );
+};
 export default App;
