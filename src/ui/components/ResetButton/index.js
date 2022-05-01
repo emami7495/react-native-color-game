84f7ae609f9a45d12405782ffe70751f71fd24eb
Mostafa Emami <emami7495@gmail.com>
2022-05-02 02:00:29 +0430

    Add animation to Reset & Timer Buttons

--- src/ui/components/ResetButton/index.js
+++ src/ui/components/ResetButton/index.js
@@ -1,29 +1,36 @@
 import React, {memo} from 'react';
 import {Text, TouchableOpacity} from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 
 const ResetButton = ({isWin, onPress}) => {
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={{
-        margin: 24,
-        width: '50%',
-        borderRadius: 4,
-        backgroundColor: isWin?'#49a910':'#ff5a5a',
-      }}>
-      <Text
+    <Animatable.View
+      duration={2500}
+      animation="fadeInUp"
+      useNativeDriver={true}
+    >
+      <TouchableOpacity
+        onPress={onPress}
         style={{
-          margin: 16,
-          fontSize: 18,
-          fontWeight: '300',
-          textAlign: 'center',
-          paddingHorizontal: 16,
-          color: isWin?'white':'black',
+          margin: 24,
+          width: '50%',
+          borderRadius: 4,
+          backgroundColor: isWin?'#49a910':'#ff5a5a',
         }}>
-        {isWin?'Start Again':'Reset'}
-      </Text>
-    </TouchableOpacity>
+        <Text
+          style={{
+            margin: 16,
+            fontSize: 18,
+            fontWeight: '300',
+            textAlign: 'center',
+            paddingHorizontal: 16,
+            color: isWin?'white':'black',
+          }}>
+          {isWin?'Start Again':'Reset'}
+        </Text>
+      </TouchableOpacity>
+    </Animatable.View>
   );
 };
 export default memo(ResetButton);
--- src/ui/components/TimerButton/index.js
+++ src/ui/components/TimerButton/index.js
@@ -1,29 +1,36 @@
 import React, {memo} from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 import {getTime} from '../../../utils/utils';
+import * as Animatable from 'react-native-animatable';
 
 
 const TimerButton = ({isWin, winTime, time, onPress}) => {
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={{
-        width: '50%',
-        marginVertical: 24, borderRadius: 4,
-        backgroundColor: isWin?'#49a910':'#ff5a5a',
-      }}>
-      <Text
+    <Animatable.View
+      duration={2500}
+      useNativeDriver={true}
+      animation="fadeInDown"
+    >
+      <TouchableOpacity
+        onPress={onPress}
         style={{
-          margin: 16,
-          fontSize: 18,
-          fontWeight: '300',
-          textAlign: 'center',
-          paddingHorizontal: 16,
-          color: isWin?'white':'black',
+          width: '50%',
+          marginVertical: 24, borderRadius: 4,
+          backgroundColor: isWin?'#49a910':'#4c93da',
         }}>
-        {isWin?getTime(winTime+1):getTime(time)}
-      </Text>
-    </TouchableOpacity>
+        <Text
+          style={{
+            margin: 16,
+            fontSize: 18,
+            fontWeight: '300',
+            textAlign: 'center',
+            paddingHorizontal: 16,
+            color: isWin?'white':'black',
+          }}>
+          {isWin?getTime(winTime+1):getTime(time)}
+        </Text>
+      </TouchableOpacity>
+    </Animatable.View>
   );
 };
 export default memo(TimerButton);
