f9b7725890637d3a50aab89a1cde9cef8599012b
Mostafa Emami <emami7495@gmail.com>
2022-05-06 19:18:52 +0430

    Fix Text component issue in ios

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,7 +1,8 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
 
 const APP_CONSTANTS = {
   randomColor: require('randomcolor'),
   screenWidth: Dimensions.get('window').width,
+  os: Platform.OS,
 };
 export default APP_CONSTANTS;
--- src/ui/kit/Text/index.tsx
+++ src/ui/kit/Text/index.tsx
@@ -1,9 +1,13 @@
 import React, { memo } from 'react';
 import { Text as T } from 'react-native';
 import styles from './styles';
+import APP_CONSTANTS from '../../../constants/app';
 
 // Text
 function Text({ style, ...props }:any) {
+  if (APP_CONSTANTS.os === 'ios') {
+    return <T style={style} {...props} />;
+  }
   return <T style={[styles.root, style]} {...props} />;
 }
 
